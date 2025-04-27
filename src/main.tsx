5b819b8aad84564fa5ca8239a771b3644773282e
iamhuunter <131743511+iamhuunter@users.noreply.github.com>
2025-04-28 02:48:31 +0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import VoiceRoomApp from './VoiceRoomApp'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <VoiceRoomApp />
+  </React.StrictMode>,
+)
